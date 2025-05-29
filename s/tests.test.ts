c433855ff203c1cbe144da65d1f2d122e85c366f
Chase Moskal <chasemoskal@gmail.com>
2025-05-29 13:26:19 -0700

    activate: bucket tests

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -1,8 +1,10 @@
 
 import {Science} from "@e280/science"
+import buckets from "./buckets/buckets.test.js"
 import nametag from "./nametag/nametag.test.js"
 
 await Science.run({
+	buckets,
 	nametag,
 })
 
