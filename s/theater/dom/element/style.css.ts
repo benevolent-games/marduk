72d2eb3a0c1134e15c055358514c920a399e1dfb
Chase Moskal <chasemoskal@gmail.com>
2025-06-04 18:40:11 -0700

    tweak: theater styles

--- s/theater/dom/element/style.css.ts
+++ s/theater/dom/element/style.css.ts
@@ -4,15 +4,15 @@ export default css`
 
 :host {
 	display: block;
-	width: 20em;
-	height: 10em;
-	border: 1px solid #fff4;
+	width: 100%;
+	height: 100%;
 }
 
 canvas {
+	display: block;
 	width: 100%;
 	height: 100%;
-	background: #400;
+	background: #000;
 }
 
 `
--- s/theater/dom/element/view.ts
+++ s/theater/dom/element/view.ts
@@ -4,6 +4,7 @@ import styleCss from "./style.css.js"
 import {Frontstage} from "../frontstage.js"
 
 export const TheaterView = shadowView(use => (frontstage: Frontstage<any>) => {
+	use.name("theater")
 	use.styles(styleCss)
 	use.deferOnce(() => frontstage.rezzer.recalibrate())
 	return frontstage.canvas
